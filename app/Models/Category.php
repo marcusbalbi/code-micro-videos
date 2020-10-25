8022a783543402f84fc610a25b80514c592da8e5
Marcus Balbi <balbimarcus@gmail.com>
2020-10-25 20:19:51 -0300

    softDeletes

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -3,12 +3,17 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Category extends Model
 {
+    use SoftDeletes;
+
     protected $fillable = [
         'name',
         'description',
         'is_active'
     ];
+
+    protected $dates = ['deleted_at'];
 }
