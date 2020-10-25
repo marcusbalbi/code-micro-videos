4734fda0181d25e91a34bea357492e35be172d7f
Marcus Balbi <balbimarcus@gmail.com>
2020-10-25 20:56:10 -0300

    add trait and Genre Model

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -7,7 +7,7 @@ use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Category extends Model
 {
-    use SoftDeletes;
+    use SoftDeletes, Traits\uuid;
 
     protected $fillable = [
         'name',
