7fc514ee107542bb714797e1d34f81aa8c07b07d
Marcus Balbi <balbimarcus@gmail.com>
2020-10-25 21:05:10 -0300

    Genre Model done and database

--- app/Models/Genre.php
+++ app/Models/Genre.php
@@ -3,8 +3,21 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Genre extends Model
 {
-    //
+    use SoftDeletes, Traits\uuid;
+
+    protected $fillable = [
+        'name',
+        'is_active'
+    ];
+
+    protected $casts = [
+        'id' => 'string',
+        'is_active' => 'boolean'
+    ];
+
+    protected $dates = ['deleted_at'];
 }
