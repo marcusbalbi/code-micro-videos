a717a83ae12788fbf200e9cc65fb1f97bdd07dc9
Marcus Balbi <balbimarcus@gmail.com>
2020-10-25 21:20:52 -0300

    Genre Controller working

---
+++ app/Http/Controllers/Api/GenreController.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Models\Genre;
+use App\Http\Controllers\Controller;
+use App\Http\Requests\GenreRequest;
+
+class GenreController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return Genre::all();
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(GenreRequest $request)
+    {
+        return Genre::create($request->all());
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Genre  $genre
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Genre $genre)
+    {
+        return $genre;
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Genre  $genre
+     * @return \Illuminate\Http\Response
+     */
+    public function update(GenreRequest $request, Genre $genre)
+    {
+        $genre->update($request->all());
+        return $genre;
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Genre  $genre
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Genre $genre)
+    {
+        $genre->delete();
+        return response()->noContent();
+    }
+}
