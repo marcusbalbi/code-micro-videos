13405fa4a3e736f0ecc5198a4036194886610b7e
Marcus Balbi <balbimarcus@gmail.com>
2020-10-27 22:53:32 -0300

    unit test genre

---
+++ tests/Unit/Models/GenreTest.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace Tests\Unit\Models;
+
+use Tests\TestCase;
+use \App\Models\Genre;
+use App\Models\Traits\Uuid;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class GenreTest extends TestCase
+{
+    // use DatabaseMigrations;
+
+    private $genre;
+
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+        $this->genre = new Genre();
+    }
+
+    public function testFillable()
+    {
+        $fillable = ['name', 'is_active'];
+        $this->assertEquals($fillable, $this->genre->getFillable());
+    }
+
+    public function testHasCorrectTraits()
+    {
+        $traits = [
+            SoftDeletes::class,
+            Uuid::class
+        ];
+        $useTraits = array_keys(class_uses(Genre::class));
+        $this->assertEquals($useTraits, $traits);
+    }
+
+    public function testCasts()
+    {
+        $casts = [
+            'id' => 'string',
+            'is_active' => 'boolean'
+        ];
+        $this->assertEquals($casts, $this->genre->getCasts());
+    }
+
+    public function testIncrementing()
+    {
+        $this->assertFalse($this->genre->incrementing);
+    }
+    public function testDates()
+    {
+        $dates = ['deleted_at', 'created_at', 'updated_at'];
+        foreach ($dates as $date) {
+            $this->assertContains($date, $this->genre->getDates());
+        }
+        $this->assertCount(count($dates), $this->genre->getDates());
+    }
+}
