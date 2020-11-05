5d07a410b85edd088fa66396c988e3eab43934f7
Marcus Balbi <balbimarcus@gmail.com>
2020-11-04 22:23:40 -0300

    changes

--- app/Http/Controllers/Api/CastMemberController.php
+++ app/Http/Controllers/Api/CastMemberController.php
@@ -16,43 +16,12 @@ class CastMemberController extends BasicCrudController
     {
         return [
             'name' => 'required|max:255',
-            'type' => 'numeric|in:1,2'
+            'type' => 'required|numeric|in:1,2'
         ];
     }
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  \App\Models\CastMember  $castMember
-     * @return \Illuminate\Http\Response
-     */
-    public function show(CastMember $castMember)
+    protected function rulesUpdate()
     {
-        return $castMember;
-    }
-
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \App\Models\CastMember  $castMember
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, CastMember $castMember)
-    {
-        $castMember->update($request->all());
-        return $castMember;
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  \App\Models\CastMember  $castMember
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy(CastMember $castMember)
-    {
-        $castMember->delete();
-        return response()->noContent();
+        return $this->rulesStore();
     }
 }
