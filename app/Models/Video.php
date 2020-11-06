54e3c514a6cd0ae63b0fea3dff791805cb0cfbed
Marcus Balbi <balbimarcus@gmail.com>
2020-11-05 21:10:08 -0300

    Models

---
+++ app/Models/Video.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Models;
+
+use App\Models\Traits\Uuid;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Video extends Model
+{
+    use SoftDeletes, Uuid;
+
+    protected $fillable = [
+        'title',
+        'description',
+        'year_launched',
+        'opened',
+        'rating',
+        'duration',
+    ];
+
+    protected $dates = ['deleted_at'];
+
+    protected $casts = [
+        'id' => 'string',
+        'opened' => 'boolean',
+        'year_launched' => 'boolean',
+        'duration' => 'integer',
+    ];
+
+    public $incrementing = false;
+}
