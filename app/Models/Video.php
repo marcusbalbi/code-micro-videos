c350141600668e512854029730479ae8fa0a2c6f
Marcus Balbi <balbimarcus@gmail.com>
2020-11-05 22:23:30 -0300

    migrations on relations

--- app/Models/Video.php
+++ app/Models/Video.php
@@ -31,4 +31,12 @@ class Video extends Model
     ];
 
     public $incrementing = false;
+
+    public function categories() {
+        return $this->belongsToMany(Category::class);
+    }
+
+    public function genres() {
+        return $this->belongsToMany(Genre::class);
+    }
 }
