5b7fc42dbe7111152d2c4ff9dabadf71cd6a7c93
Marcus Balbi <balbimarcus@gmail.com>
2020-11-17 22:00:57 -0300

    Test oldFiles

--- tests/Stubs/Models/UploadFilesStub.php
+++ tests/Stubs/Models/UploadFilesStub.php
@@ -10,11 +10,26 @@ class UploadFilesStub extends Model
 {
     use UploadFiles;
 
-    // protected $table = 'upload_file_stub';
+    protected $table = 'upload_file_stub';
 
-    // protected $fillable = ['name', 'file1', 'file2'];
+    protected $fillable = ['name', 'file1', 'file2'];
 
     public static $fileFields = ['file1', 'file2'];
+
+    public static function makeTable()
+    {
+        \Schema::create('upload_file_stub', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('name');
+            $table->text('file1')->nullable();
+            $table->text('file2')->nullable();
+            $table->timestamps();
+        });
+    }
+    public static function dropTable()
+    {
+        \Schema::dropIfExists('upload_file_stub');
+    }
 
     protected function uploadDir()
     {
