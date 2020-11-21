993650151f277e27a41228ba5c6ff40eb1535b98
Marcus Balbi <marcus.balbi@rgsistemas.com.br>
2020-11-21 11:10:13 -0300

    resolvendo problema em storage

--- .docker/entrypoint.sh
+++ .docker/entrypoint.sh
@@ -3,10 +3,11 @@
 #On error no such file entrypoint.sh, execute in terminal - dos2unix .docker\entrypoint.sh
 cp .env.example .env
 cp .env.example.testing .env.testing
-chown -R www-data:www-data .
+#chown -R www-data:www-data .
+chmod 777 -R storage/
 composer --version
 composer install
 php artisan key:generate
 php artisan migrate
-
+php artisan cache:clear
 php-fpm
