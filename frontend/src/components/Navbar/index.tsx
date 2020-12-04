f86c62174ef298fb075eaba23e7f3f020ad811c9
Marcus Balbi <balbimarcus@gmail.com>
2020-12-03 21:54:29 -0300

    material ui integrated and appbar

---
+++ frontend/src/components/Navbar/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import {AppBar, Toolbar} from '@material-ui/core'
+
+export const Navbar: React.FC = () => {
+  return (
+    <AppBar>
+      <Toolbar>
+        texto
+      </Toolbar>
+    </AppBar>
+  )
+}
