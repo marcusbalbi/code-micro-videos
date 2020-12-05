033b2ebe265bcf58bed52954e6dc5edba67a45f8
Marcus Balbi <balbimarcus@gmail.com>
2020-12-05 12:21:18 -0300

    page created

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,18 @@
+import { Box } from '@material-ui/core';
 import React from 'react';
 import './App.css';
 import { Navbar } from './components/Navbar';
+import { Page } from './components/Page';
 function App() {
   return (
-    <Navbar />
+    <React.Fragment>
+      <Navbar />
+      <Box paddingTop={'70px'}>
+        <Page title={"Categorias"}>
+          Conteíudo
+        </Page>
+      </Box>
+    </React.Fragment>
   );
 }
 
