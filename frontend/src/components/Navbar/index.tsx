e2f093196c5ac07c416e53cf0aa6427ba8093ba5
Marcus Balbi <balbimarcus@gmail.com>
2020-12-04 21:47:03 -0300

    style for toolbar

--- frontend/src/components/Navbar/index.tsx
+++ frontend/src/components/Navbar/index.tsx
@@ -1,12 +1,27 @@
 import React from 'react'
-import {AppBar, Toolbar} from '@material-ui/core'
+import {AppBar, Button, makeStyles, Toolbar, Typography} from '@material-ui/core'
+import logo from '../../static/img/logo.png'
+
+const useStyles = makeStyles({
+  toolbar: {
+    backgroundColor: '#000000',
+  },
+  title: {
+    flexGrow: 1,
+    textAlign: 'center'
+  }
+})
 
 export const Navbar: React.FC = () => {
+  const classes = useStyles()
   return (
     <AppBar>
-      <Toolbar>
-        texto
+      <Toolbar className={classes.toolbar}>
+        <Typography className={classes.title}>
+          <img src={logo} alt="Logo" />
+        </Typography>
+        <Button color="inherit">Login</Button>
       </Toolbar>
     </AppBar>
-  )
+  );
 }
