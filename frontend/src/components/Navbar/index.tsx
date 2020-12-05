b1af78c2616a6171b58ef11166d5deda1bece3a8
Marcus Balbi <balbimarcus@gmail.com>
2020-12-05 11:56:41 -0300

    separate Menu from Navbar

--- frontend/src/components/Navbar/index.tsx
+++ frontend/src/components/Navbar/index.tsx
@@ -1,18 +1,15 @@
-import React, { useState } from "react";
+import React from "react";
 import {
   AppBar,
   Button,
   makeStyles,
   Toolbar,
   Theme,
-  Typography,
-  IconButton,
-  Menu,
-  MenuItem,
+  Typography
 } from "@material-ui/core";
-import { Menu as MenuIcon } from "@material-ui/icons";
 
 import logo from "../../static/img/logo.png";
+import { Menu } from "./Menu";
 
 const useStyles = makeStyles((theme: Theme) => {
   return {
@@ -34,42 +31,11 @@ const useStyles = makeStyles((theme: Theme) => {
 
 export const Navbar: React.FC = () => {
   const classes = useStyles();
-  const [anchorEl, setAnchorEl] = useState(null);
-  const open = Boolean(anchorEl);
-
-  const handleOpen = (event: any) => {
-    setAnchorEl(event.currentTarget);
-  };
-  const handleOnClose = () => {
-    setAnchorEl(null);
-  };
 
   return (
     <AppBar>
       <Toolbar className={classes.toolbar}>
-        <IconButton
-          edge="start"
-          color="inherit"
-          arial-label="open-drawer"
-          arial-controls="menu-appbar"
-          aria-haspopup={true}
-          onClick={handleOpen}
-        >
-          <MenuIcon />
-        </IconButton>
-        <Menu
-          id="menu-appbar"
-          open={open}
-          anchorEl={anchorEl}
-          onClose={handleOnClose}
-          anchorOrigin={{ vertical: "bottom", horizontal: "center" }}
-          transformOrigin={{ vertical: "top", horizontal: "center" }}
-          getContentAnchorEl={null}
-        >
-          <MenuItem>Categorias</MenuItem>
-          <MenuItem>Membros</MenuItem>
-          <MenuItem>Gêneros</MenuItem>
-        </Menu>
+        <Menu />
         <Typography className={classes.title}>
           <img className={classes.logo} src={logo} alt="Logo" />
         </Typography>
