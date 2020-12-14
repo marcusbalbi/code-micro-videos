e4db574e3cab77c3385183a5f81a9ecea5b449b0
Marcus Balbi <balbimarcus@gmail.com>
2020-12-14 19:49:00 -0300

    routes map created

---
+++ frontend/src/routes/index.ts
@@ -0,0 +1,24 @@
+import { RouteProps } from "react-router-dom";
+import { CategoryList } from "../pages/category/CategoryList";
+import { Dashboard } from "../pages/Dashboard";
+
+interface MyRouteProps extends RouteProps {
+  label: string;
+}
+
+const routes: Array<MyRouteProps> = [
+  {
+    label: "Dashboard",
+    path: "/",
+    component: Dashboard,
+    exact: true,
+  },
+  {
+    label: "Listar Categorias",
+    path: "/categores",
+    component: CategoryList,
+    exact: true,
+  },
+];
+
+export default routes;
