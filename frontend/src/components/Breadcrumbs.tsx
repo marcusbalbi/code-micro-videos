b6605a05423fb86556318c8a75ec2157dd3bbf51
Marcus Balbi <balbimarcus@gmail.com>
2020-12-15 18:33:11 -0300

    remove unsed code

--- frontend/src/components/Breadcrumbs.tsx
+++ frontend/src/components/Breadcrumbs.tsx
@@ -2,21 +2,10 @@
 import React from "react";
 import { makeStyles, Theme, createStyles } from "@material-ui/core/styles";
 import Link, { LinkProps } from "@material-ui/core/Link";
-import ListItem from "@material-ui/core/ListItem";
-import Collapse from "@material-ui/core/Collapse";
-import ListItemText from "@material-ui/core/ListItemText";
 import Typography from "@material-ui/core/Typography";
-import ExpandLess from "@material-ui/icons/ExpandLess";
-import ExpandMore from "@material-ui/icons/ExpandMore";
 import MuiBreadcrumbs from "@material-ui/core/Breadcrumbs";
-import { Route, MemoryRouter } from "react-router";
+import { Route } from "react-router";
 import { Link as RouterLink } from "react-router-dom";
-import { Omit } from "@material-ui/types";
-
-interface ListItemLinkProps extends LinkProps {
-  to: string;
-  open?: boolean;
-}
 
 const breadcrumbNameMap: { [key: string]: string } = {
   "/inbox": "Inbox",
@@ -25,20 +14,6 @@ const breadcrumbNameMap: { [key: string]: string } = {
   "/spam": "Spam",
   "/drafts": "Drafts",
 };
-
-function ListItemLink(props: Omit<ListItemLinkProps, "ref">) {
-  const { to, open, ...other } = props;
-  const primary = breadcrumbNameMap[to];
-
-  return (
-    <li>
-      <ListItem button component={RouterLink} to={to} {...other}>
-        <ListItemText primary={primary} />
-        {open != null ? open ? <ExpandLess /> : <ExpandMore /> : null}
-      </ListItem>
-    </li>
-  );
-}
 
 const useStyles = makeStyles((theme: Theme) =>
   createStyles({
@@ -68,43 +43,36 @@ const LinkRouter = (props: LinkRouterProps) => (
 
 export default function Breadcrumbs() {
   const classes = useStyles();
-  const [open, setOpen] = React.useState(true);
-
-  const handleClick = () => {
-    setOpen((prevOpen) => !prevOpen);
-  };
 
   return (
-    <MemoryRouter initialEntries={["/inbox"]} initialIndex={0}>
-      <div className={classes.root}>
-        <Route>
-          {({ location }) => {
-            const pathnames = location.pathname.split("/").filter((x) => x);
+    <div className={classes.root}>
+      <Route>
+        {({ location }) => {
+          const pathnames = location.pathname.split("/").filter((x) => x);
 
-            return (
-              <MuiBreadcrumbs aria-label="breadcrumb">
-                <LinkRouter color="inherit" to="/">
-                  Home
-                </LinkRouter>
-                {pathnames.map((value, index) => {
-                  const last = index === pathnames.length - 1;
-                  const to = `/${pathnames.slice(0, index + 1).join("/")}`;
+          return (
+            <MuiBreadcrumbs aria-label="breadcrumb">
+              <LinkRouter color="inherit" to="/">
+                Home
+              </LinkRouter>
+              {pathnames.map((value, index) => {
+                const last = index === pathnames.length - 1;
+                const to = `/${pathnames.slice(0, index + 1).join("/")}`;
 
-                  return last ? (
-                    <Typography color="textPrimary" key={to}>
-                      {breadcrumbNameMap[to]}
-                    </Typography>
-                  ) : (
-                    <LinkRouter color="inherit" to={to} key={to}>
-                      {breadcrumbNameMap[to]}
-                    </LinkRouter>
-                  );
-                })}
-              </MuiBreadcrumbs>
-            );
-          }}
-        </Route>
-      </div>
-    </MemoryRouter>
+                return last ? (
+                  <Typography color="textPrimary" key={to}>
+                    {breadcrumbNameMap[to]}
+                  </Typography>
+                ) : (
+                  <LinkRouter color="inherit" to={to} key={to}>
+                    {breadcrumbNameMap[to]}
+                  </LinkRouter>
+                );
+              })}
+            </MuiBreadcrumbs>
+          );
+        }}
+      </Route>
+    </div>
   );
 }
