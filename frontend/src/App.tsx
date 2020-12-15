6fcc104e9c0404cef3ac2c916dd9fbdd9c17ad98
Marcus Balbi <balbimarcus@gmail.com>
2020-12-14 22:41:16 -0300

    first contact with breadcrumbs

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,6 +2,7 @@ import { Box } from "@material-ui/core";
 import React from "react";
 import { BrowserRouter } from "react-router-dom";
 import "./App.css";
+import Breadcrumbs from "./components/Breadcrumbs";
 import { Navbar } from "./components/Navbar";
 import { AppRouter } from "./routes/AppRouter";
 function App() {
@@ -10,6 +11,7 @@ function App() {
       <BrowserRouter>
         <Navbar />
         <Box paddingTop={"70px"}>
+          <Breadcrumbs />
           <AppRouter />
         </Box>
       </BrowserRouter>
