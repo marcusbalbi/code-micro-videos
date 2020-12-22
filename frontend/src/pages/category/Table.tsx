5d6473c3fc42053c9637c7598375acce3ff9bacf
Marcus Balbi <balbimarcus@gmail.com>
2020-12-22 19:22:16 -0300

    add date-fns and format date

--- frontend/src/pages/category/Table.tsx
+++ frontend/src/pages/category/Table.tsx
@@ -3,6 +3,9 @@ import MUIDataTable, { MUIDataTableColumn } from "mui-datatables";
 import React, { useEffect, useState } from "react";
 import { httpVideo } from "../../util/http";
 
+import format from "date-fns/format";
+import parseISO from "date-fns/parseISO";
+
 const columnsDefinition: MUIDataTableColumn[] = [
   {
     name: "name",
@@ -23,6 +26,11 @@ const columnsDefinition: MUIDataTableColumn[] = [
   {
     name: "created_at",
     label: "Criado em",
+    options: {
+      customBodyRender: (value, tableMeta, updateValue) => {
+        return <span>{format(parseISO(value), "dd/MM/yyyy")}</span>;
+      },
+    },
   },
 ];
 
