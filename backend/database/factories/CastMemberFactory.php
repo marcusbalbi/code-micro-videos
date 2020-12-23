0548b27ac16d02f9327df7773f6631311980aaec
Marcus Balbi <balbimarcus@gmail.com>
2020-12-22 21:26:14 -0300

    fix problem on CatMember seed

--- backend/database/factories/CastMemberFactory.php
+++ backend/database/factories/CastMemberFactory.php
@@ -8,6 +8,6 @@ use Faker\Generator as Faker;
 $factory->define(CastMember::class, function (Faker $faker) {
     return [
         'name' => $faker->firstName,
-        'type' => array_rand([CastMember::TYPE_ACTOR, CastMember::TYPE_DIRECTOR])
+        'type' => array_rand([CastMember::TYPE_DIRECTOR, CastMember::TYPE_ACTOR]) + 1
     ];
 });
