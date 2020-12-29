0276f1d352b9200cfa791f3e44e87a07dc158e8c
Marcus Balbi <balbimarcus@gmail.com>
2020-12-28 22:00:53 -0300

    CategoryForm created

--- frontend/src/pages/category/Form.tsx
+++ frontend/src/pages/category/Form.tsx
@@ -1,10 +1,49 @@
-import { TextField } from "@material-ui/core";
+import {
+  Box,
+  Button,
+  Checkbox,
+  TextField,
+  ButtonProps,
+  makeStyles,
+  Theme,
+} from "@material-ui/core";
 import React from "react";
 
+const useStyles = makeStyles((theme: Theme) => {
+  return {
+    submit: {
+      margin: theme.spacing(1),
+    },
+  };
+});
+
 export const Form = () => {
+  const classes = useStyles();
+  const buttonProps: ButtonProps = {
+    variant: "outlined",
+    size: "medium",
+    className: classes.submit,
+  };
   return (
     <form>
-      <TextField></TextField>
+      <TextField name="name" label="Nome" fullWidth variant="outlined" />
+      <TextField
+        name="description"
+        label="Descrição"
+        margin="normal"
+        variant="outlined"
+        fullWidth
+        multiline
+        rows={5}
+      />
+      <Checkbox name="is_active" />
+      Ativo?
+      <Box dir={"rtl"}>
+        <Button {...buttonProps}>Salvar</Button>
+        <Button {...buttonProps} type="submit">
+          Salvar e continuar editando
+        </Button>
+      </Box>
     </form>
   );
 };
