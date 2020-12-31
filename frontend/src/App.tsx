e84680c7428593c556bb388bf3d1b62e997b61d2
Marcus Balbi <balbimarcus@gmail.com>
2020-12-30 22:08:52 -0300

    add theme

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,20 +1,23 @@
-import { Box } from "@material-ui/core";
+import { Box, MuiThemeProvider } from "@material-ui/core";
 import React from "react";
 import { BrowserRouter } from "react-router-dom";
 import "./App.css";
 import Breadcrumbs from "./components/Breadcrumbs";
 import { Navbar } from "./components/Navbar";
 import { AppRouter } from "./routes/AppRouter";
+import theme from "./theme";
 function App() {
   return (
     <React.Fragment>
-      <BrowserRouter>
-        <Navbar />
-        <Box paddingTop={"70px"}>
-          <Breadcrumbs />
-          <AppRouter />
-        </Box>
-      </BrowserRouter>
+      <MuiThemeProvider theme={theme}>
+        <BrowserRouter>
+          <Navbar />
+          <Box paddingTop={"70px"}>
+            <Breadcrumbs />
+            <AppRouter />
+          </Box>
+        </BrowserRouter>
+      </MuiThemeProvider>
     </React.Fragment>
   );
 }
---
+++ frontend/src/theme.ts
@@ -0,0 +1,16 @@
+import { createMuiTheme } from "@material-ui/core";
+
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      main: "#79aec7",
+      contrastText: "#FFF",
+    },
+    secondary: {
+      main: "#4db5ab",
+      contrastText: "#FFF",
+    },
+  },
+});
+
+export default theme;
