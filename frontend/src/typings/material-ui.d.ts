a9309e09fadf23add1132e2defa8c4cdb0c48215
Marcus Balbi <balbimarcus@gmail.com>
2021-01-04 22:13:23 -0300

    new types

---
+++ frontend/src/typings/material-ui.d.ts
@@ -0,0 +1,15 @@
+import { ComponentNameToClassKey } from "@material-ui/core/styles/overrides";
+
+declare module "@material-ui/core/styles/overrides" {
+  interface ComponentNameToClassKey {
+    MUIDataTable: any;
+    MUIDataTableToolbar: any;
+    MUIDataTableHeadCell: any;
+    MuiDataTableSortLabel: any;
+    MUIDataTableSelectCell: any;
+    MUIDataTableBodyCell: any;
+    MUIDataTableToolbarSelect: any;
+    MUIDataTableBodyRow: any;
+    MuiDataTablePagination: any;
+  }
+}
