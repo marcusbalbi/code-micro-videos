a5f5ef2078d84912a9c807d39944c1e5ee9c0f56
Marcus Balbi <balbimarcus@gmail.com>
2021-01-04 22:13:39 -0300

    change MUIDatatable colors

--- frontend/src/theme.ts
+++ frontend/src/theme.ts
@@ -1,26 +1,46 @@
 import { createMuiTheme } from "@material-ui/core";
+import { PaletteOptions, SimplePaletteColorOptions } from "@material-ui/core/styles/createPalette";
+
+const palette: PaletteOptions = {
+  primary: {
+    main: "#79aec7",
+    contrastText: "#FFF",
+  },
+  secondary: {
+    main: "#4db5ab",
+    contrastText: "#FFF",
+  },
+  background: {
+    default: "#fafafa",
+  },
+};
 
 const theme = createMuiTheme({
-  palette: {
-    primary: {
-      main: "#79aec7",
-      contrastText: "#FFF",
-    },
-    secondary: {
-      main: "#4db5ab",
-      contrastText: "#FFF",
-    },
-    background: {
-      default: '#fafafa'
-    },
-    
-  },
+  palette,
   overrides: {
-    // MuiFormLabel: {
-    //   root: {
-    //     backgroundColor: "red",
-    //   },
-    // },
+    MUIDataTable: {
+      paper: {
+        boxShadow: "none",
+      },
+    },
+    MUIDataTableToolbar: {
+      root: {
+        minHeight: "58px",
+        backgroundColor: palette!.background!.default,
+      },
+      icon: {
+        color: (palette!.primary as SimplePaletteColorOptions).main,
+        "&:hover, &:active, &:focus": {
+          color: "#055a52",
+        },
+      },
+      iconActive: {
+        color: "#055a52",
+        "&:hover, &:active, &:focus": {
+          color: "#055a52",
+        },
+      },
+    },
   },
 });
 
