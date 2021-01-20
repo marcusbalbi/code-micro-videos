c5b66a6b20fdb19e779e6f98c16ba9e32d6bc8fa
Marcus Balbi <balbimarcus@gmail.com>
2021-01-19 21:24:06 -0300

    add width all tables

--- frontend/src/pages/cast-member/Table.tsx
+++ frontend/src/pages/cast-member/Table.tsx
@@ -1,24 +1,33 @@
-import { MUIDataTableColumn } from "mui-datatables";
 import React, { useEffect, useState } from "react";
 
 import format from "date-fns/format";
 import parseISO from "date-fns/parseISO";
 import httpCastMember from "../../util/http/http-cast-member";
 import { CastMember, ListResponse } from "../../util/dto";
-import DefaultTable from "../../components/Table";
+import DefaultTable, { TableColumns } from "../../components/Table";
 const CastMemberTypeMap = {
   1: "Diretor",
   2: "Ator",
 };
 
-const columnsDefinition: MUIDataTableColumn[] = [
+const columnsDefinition: TableColumns[] = [
+  {
+    name: "id",
+    label: "ID",
+    width: "30%",
+    options: {
+      sort: false,
+    },
+  },
   {
     name: "name",
     label: "nome",
+    width: "24%",
   },
   {
     name: "type",
     label: "Tipo",
+    width: "20%",
     options: {
       customBodyRender: (value, tableMeta, updateValue) => {
         return CastMemberTypeMap[value];
@@ -28,10 +37,19 @@ const columnsDefinition: MUIDataTableColumn[] = [
   {
     name: "created_at",
     label: "Criado em",
+    width: "10%",
     options: {
       customBodyRender: (value, tableMeta, updateValue) => {
         return <span>{format(parseISO(value), "dd/MM/yyyy")}</span>;
       },
+    },
+  },
+  {
+    name: "actions",
+    label: "Ações",
+    width: `16%`,
+    options: {
+      sort: false,
     },
   },
 ];
--- frontend/src/pages/category/Table.tsx
+++ frontend/src/pages/category/Table.tsx
@@ -1,4 +1,3 @@
-import { MUIDataTableColumn } from "mui-datatables";
 import React, { useEffect, useState } from "react";
 
 import format from "date-fns/format";
@@ -6,15 +5,25 @@ import parseISO from "date-fns/parseISO";
 import httpCategory from "../../util/http/http-category";
 import { BadgeNo, BadgeYes } from "../../components/Badge";
 import { Category, ListResponse } from "../../util/dto";
-import DefaultTable from "../../components/Table";
-const columnsDefinition: MUIDataTableColumn[] = [
+import DefaultTable, { TableColumns } from "../../components/Table";
+const columnsDefinition: TableColumns[] = [
+  {
+    name: "id",
+    label: "ID",
+    width: "30%",
+    options: {
+      sort: false,
+    },
+  },
   {
     name: "name",
     label: "nome",
+    width: "40%",
   },
   {
     name: "is_active",
     label: "Ativo?",
+    width: "4%",
     options: {
       customBodyRender: (value, tableMeta, updateValue) => {
         if (value === true) {
@@ -27,10 +36,19 @@ const columnsDefinition: MUIDataTableColumn[] = [
   {
     name: "created_at",
     label: "Criado em",
+    width: "10%",
     options: {
       customBodyRender: (value, tableMeta, updateValue) => {
         return <span>{format(parseISO(value), "dd/MM/yyyy")}</span>;
       },
+    },
+  },
+  {
+    name: "actions",
+    label: "Ações",
+    width: `16%`,
+    options: {
+      sort: false,
     },
   },
 ];
