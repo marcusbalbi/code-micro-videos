1bbae96fc545071b1378181c7a5b2e9bdba5c5d1
Marcus Balbi <balbimarcus@gmail.com>
2021-02-08 18:28:06 -0300

    GenreFilter

---
+++ backend/app/ModelFilters/GenreFilter.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\ModelFilters;
+
+use EloquentFilter\ModelFilter;
+use Illuminate\Database\Eloquent\Builder;
+
+class GenreFilter extends ModelFilter
+{
+    protected $sortable = ['name', 'type', 'created_at'];
+
+    public function search($search)
+    {
+        $this->where('name', 'LIKE', "%$search%");
+    }
+
+    public function categories($categories)
+    {
+        $idsOrNames = explode(",", $categories);
+
+        $this->whereHas('categories', function (Builder $query) use ($idsOrNames) {
+            $query->whereIn("id", $idsOrNames)
+            ->orWhereIn('name', $idsOrNames);
+        });
+    }
+}
