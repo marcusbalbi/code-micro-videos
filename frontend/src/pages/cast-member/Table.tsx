0aafd2c6ad4fc4142d2e49fb87199e4fdd6356d8
Marcus Balbi <balbimarcus@gmail.com>
2021-02-09 20:56:06 -0300

    upgrade cast members to use useFilter

--- frontend/src/pages/cast-member/Table.tsx
+++ frontend/src/pages/cast-member/Table.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useCallback, useEffect, useRef, useState } from "react";
 
 import format from "date-fns/format";
 import parseISO from "date-fns/parseISO";
@@ -6,13 +6,26 @@ import httpCastMember from "../../util/http/http-cast-member";
 import { CastMember, ListResponse } from "../../util/dto";
 import DefaultTable, { TableColumns } from "../../components/Table";
 import { useSnackbar } from "notistack";
+import { IconButton, Theme, ThemeProvider } from "@material-ui/core";
+import { Link } from "react-router-dom";
+import EditIcon from "@material-ui/icons/Edit";
+import { cloneDeep } from "lodash";
+import { FilterResetButton } from "../../components/Table/FilterResetButton";
+import useFilter from "../../hooks/useFilter";
+
 const CastMemberTypeMap = {
   1: "Diretor",
   2: "Ator",
 };
 
+
+const debounceTime = 300;
+const debounceTimeSearchText = 300;
+const rowsPerPage = 15;
+const rowsPerPageOptions = [15, 25, 50];
+
 const columnsDefinition: TableColumns[] = [
-  {
+ {
     name: "id",
     label: "ID",
     width: "30%",
@@ -51,44 +64,151 @@ const columnsDefinition: TableColumns[] = [
     width: `16%`,
     options: {
       sort: false,
+      customBodyRender: (value, tableMeta) => {
+        return (
+          <span>
+            <IconButton
+              color={"secondary"}
+              component={Link}
+              to={`cast-members/${tableMeta.rowData[0]}/edit`}
+            >
+              <EditIcon fontSize={"inherit"} />
+            </IconButton>
+          </span>
+        );
+      },
     },
   },
 ];
 
+function localTheme(theme: Theme) {
+  const copyTheme = cloneDeep(theme);
+  const selector = `&[data-testid^="MuiDataTableBodyCell-${
+    columnsDefinition.length - 1
+  }"]`;
+  (copyTheme.overrides as any).MUIDataTableBodyCell.root[selector] = {
+    paddingTop: "0px",
+    paddingBottom: "0px",
+  };
+
+  return copyTheme;
+}
+
 export const Table = () => {
+  const canLoad = useRef(true);
   const [castMembers, setCastMembers] = useState<CastMember[]>([]);
   const [loading, setLoading] = useState(false);
+  const {
+    filterState,
+    debouncedFilterState,
+    totalRecords,
+    setTotalRecords,
+    filterManager,
+  } = useFilter({
+    debounceTime: debounceTime,
+    rowsPerPage: rowsPerPage,
+    columns: columnsDefinition,
+    rowsPerPageOptions: rowsPerPageOptions,
+  });
   const snackbar = useSnackbar();
-  useEffect(() => {
-    let canLoad = true;
-    (async function getCastMembers() {
-      setLoading(true);
-      try {
-        const { data } = await httpCastMember.list<ListResponse<CastMember>>();
-        if (canLoad) {
-          setCastMembers(data.data);
-        }
-      } catch (error) {
-        console.log(error);
-        snackbar.enqueueSnackbar("Não foi possível carregar as informações", {
-          variant: "error",
-        });
-      } finally {
-        setLoading(false);
+
+  const getData = useCallback(async () => {
+    setLoading(true);
+    try {
+      const { data } = await httpCastMember.list<ListResponse<CastMember>>({
+        queryParams: {
+          search:
+            typeof debouncedFilterState.search === "string"
+              ? debouncedFilterState.search
+              : "",
+          page: debouncedFilterState.pagination.page,
+          per_page: debouncedFilterState.pagination.per_page,
+          sort: debouncedFilterState.order.sort,
+          dir: debouncedFilterState.order.dir,
+        },
+      });
+      if (canLoad.current) {
+        setCastMembers(data.data);
+        setTotalRecords(data.meta.total);
       }
-    })();
+    } catch (error) {
+      console.log(error);
+      if (httpCastMember.isCancelledRequest(error)) {
+        return;
+      }
+      snackbar.enqueueSnackbar("Não foi possível carregar as informações", {
+        variant: "error",
+      });
+    } finally {
+      setLoading(false);
+    }
+  }, [
+    snackbar,
+    debouncedFilterState.search,
+    debouncedFilterState.pagination.page,
+    debouncedFilterState.pagination.per_page,
+    debouncedFilterState.order,
+    setTotalRecords,
+  ]);
+
+  useEffect(() => {
+    filterManager.replaceHistory();
+    //eslint-disable-next-line
+  }, []);
+
+  useEffect(() => {
+    canLoad.current = true;
+    getData();
+    filterManager.pushHistory();
     return () => {
-      canLoad = false;
+      canLoad.current = false;
     };
-  }, [snackbar]);
+    //eslint-disable-next-line
+  }, [getData]);
 
   return (
-    <DefaultTable
-      data={castMembers}
-      loading={loading}
-      title={"Listagem de Membros de Elenco"}
-      columns={columnsDefinition}
-    />
+    <ThemeProvider theme={localTheme}>
+      <DefaultTable
+        debouncedSearchTime={debounceTimeSearchText}
+        data={castMembers}
+        loading={loading}
+        title={"Listagem de Membros de Elenco"}
+        columns={columnsDefinition}
+        options={{
+          serverSide: true,
+          searchText: filterState.search as any,
+          page: filterManager.getCorrectPage(),
+          rowsPerPage: filterState.pagination.per_page,
+          rowsPerPageOptions,
+          count: totalRecords,
+          sortOrder: {
+            name: filterState.order.sort || "NONE",
+            direction: (filterState.order.dir as any) || "asc",
+          },
+          customToolbar: () => {
+            return (
+              <FilterResetButton
+                handleClick={() => {
+                  filterManager.cleanFilter();
+                }}
+              />
+            );
+          },
+          onSearchChange: (value) => {
+            filterManager.changeSearch(value);
+          },
+          onChangePage: (page) => {
+            filterManager.changePage(page);
+          },
+          onChangeRowsPerPage: (perPage) => {
+            filterManager.changeRowsPerPage(perPage);
+          },
+          onColumnSortChange: (changedColumn, direction) => {
+            filterManager.columnSortChange(changedColumn, direction);
+          },
+        }}
+      />
+    </ThemeProvider>
   );
 };
 
