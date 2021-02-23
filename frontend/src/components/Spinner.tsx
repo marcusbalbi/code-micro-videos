8840e821164736fdc0e73a569047d876ec8a3111
Marcus Balbi <balbimarcus@gmail.com>
2021-02-23 20:44:22 -0300

    context on Spinner

--- frontend/src/components/Spinner.tsx
+++ frontend/src/components/Spinner.tsx
@@ -1,5 +1,11 @@
-import React from "react";
-import { LinearProgress, MuiThemeProvider, Theme } from "@material-ui/core";
+import React, { useContext } from "react";
+import {
+  Fade,
+  LinearProgress,
+  MuiThemeProvider,
+  Theme,
+} from "@material-ui/core";
+import LoadingContext from "./loading/LoadingContext";
 
 function makeLocalTheme(theme: Theme): Theme {
   return {
@@ -13,12 +19,15 @@ function makeLocalTheme(theme: Theme): Theme {
 }
 
 const Spinner = () => {
+  const loading = useContext(LoadingContext);
   return (
     <MuiThemeProvider theme={makeLocalTheme}>
-      <LinearProgress
-        color={"primary"}
-        style={{ position: "fixed", width: "100%", zIndex: 9999 }}
-      />
+      <Fade in={loading}>
+        <LinearProgress
+          color={"primary"}
+          style={{ position: "fixed", width: "100%", zIndex: 9999 }}
+        />
+      </Fade>
     </MuiThemeProvider>
   );
 };
