e505696c1f0921c88aa4fe9780279336724def5d
Marcus Balbi <balbimarcus@gmail.com>
2021-02-25 18:38:48 -0300

    styling snackbar upload

--- frontend/src/components/SnackbarUpload/index.tsx
+++ frontend/src/components/SnackbarUpload/index.tsx
@@ -1,15 +1,38 @@
-import React from "react";
+import React, { useState } from "react";
 import {
   Card,
   CardActions,
   Collapse,
   IconButton,
   List,
+  makeStyles,
+  Theme,
   Typography,
 } from "@material-ui/core";
 import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
+import ExpandLessIcon from "@material-ui/icons/ExpandLess";
 import CloseIcon from "@material-ui/icons/Close";
 import { useSnackbar } from "notistack";
+
+const useStyles = makeStyles((theme: Theme) => {
+  return {
+    card: {
+      width: 450,
+    },
+    cardActionRoot: {
+      padding: "8px 8px 8px 16px",
+      backgroundColor: theme.palette.primary.main,
+    },
+    title: {
+      fontWeight: "bold",
+      color: theme.palette.primary.contrastText,
+    },
+    icons: {
+      marginLeft: "auto !important",
+      color: theme.palette.primary.contrastText,
+    },
+  };
+});
 
 interface SnackbarUploadProps {
   id: string | number;
@@ -19,15 +42,22 @@ const SnackbarUpload = React.forwardRef<any, SnackbarUploadProps>(
   (props, ref) => {
     const { id } = props;
     const { closeSnackbar } = useSnackbar();
+    const [expanded, setExpanded] = useState(true);
+    const classes = useStyles({ expanded });
     return (
-      <Card ref={ref}>
-        <CardActions>
-          <Typography variant={"subtitle2"}>
+      <Card ref={ref} className={classes.card}>
+        <CardActions classes={{ root: classes.cardActionRoot }}>
+          <Typography variant={"subtitle2"} className={classes.title}>
             Fazendo Upload de 10 videos
           </Typography>
-          <div>
-            <IconButton color={"inherit"}>
-              <ExpandMoreIcon />
+          <div className={classes.icons}>
+            <IconButton
+              color={"inherit"}
+              onClick={() => {
+                setExpanded(!expanded);
+              }}
+            >
+              {expanded ? <ExpandMoreIcon /> : <ExpandLessIcon />}
             </IconButton>
             <IconButton
               onClick={() => {
@@ -39,7 +69,7 @@ const SnackbarUpload = React.forwardRef<any, SnackbarUploadProps>(
             </IconButton>
           </div>
         </CardActions>
-        <Collapse>
+        <Collapse in={expanded}>
           <List>Items</List>
         </Collapse>
       </Card>
